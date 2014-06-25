10bbbb812bea7ee48af548af6109590decefa364
Phil Campbell <philoushka@gmail.com>
2014-06-25 08:27:10 -0700

    rename to have a dash

---
+++ Install/Install-MediaSync.ps1
@@ -0,0 +1,22 @@
+#download the binary
+$mediaSyncDownloadUrl = "https://github.com/philoushka/MediaSync/releases/download/0.3/MediaSync.exe"
+$mediaSyncLocationDir = "${env:ProgramFiles(x86)}" + "\MediaSync\\"
+$file = "MediaSync.exe"
+$mediaSyncLocalFullPath = $mediaSyncLocationDir + $file
+#Create the target directory if it doesn't exist
+if (!(Test-Path $mediaSyncLocationDir)){
+	New-Item $mediaSyncLocationDir -type directory -Force
+}
+	
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($mediaSyncDownloadUrl,$mediaSyncLocalFullPath )
+
+#create a shortcut for startup
+$WshShell = New-Object -comObject WScript.Shell
+$Startup =[System.Environment]::GetFolderPath("Startup");
+$Shortcut = $WshShell.CreateShortcut("$Startup\MediaSync.lnk")
+$Shortcut.TargetPath = $mediaSyncLocalFullPath
+$Shortcut.Save()
+
+#run it
+Invoke-Item $mediaSyncLocalFullPath
