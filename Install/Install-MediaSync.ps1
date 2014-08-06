8b296e62a1706237d7e00ca9e82b8a03145f7cd5
Phil Campbell <philoushka@gmail.com>
2014-08-06 15:32:29 -0700

    download zip and extract

--- Install/Install-MediaSync.ps1
+++ Install/Install-MediaSync.ps1
@@ -2,38 +2,18 @@
 #2. Extract its contents into Program Files x86 \ MediaSync
 #3. Create a shortcut into current user's Startup dir
 #4. Run the exe
-
-#1. Download
-$mediaSyncDownloadUrl = "https://github.com/philoushka/MediaSync/releases/download/0.3/MediaSync.exe"
-$mediaSyncLocationDir = "${env:ProgramFiles(x86)}" + "\MediaSync\\"
-$zipFile ="MediaSync.zip" 
-$mediaSyncZipLocalFullPath = $mediaSyncLocationDir + $zipFile
-
-#Create the target directory if it doesn't exist
-if (!(Test-Path $mediaSyncLocationDir)){
-	New-Item $mediaSyncLocationDir -type directory -Force
-}
-	
-#download the zip
-$wc = New-Object System.Net.WebClient
-$wc.DownloadFile($mediaSyncDownloadUrl,$mediaSyncZipLocalFullPath)
-
-#download its dependants
-Expand-ZipFile($mediaSyncZipLocalFullPath, $mediaSyncLocationDir)
-
-#create a shortcut for startup
-Create-MediaSyncStartupShortcut($mediaSyncLocalFullPath)
-
-#run the exe for the user
-Invoke-Item $mediaSyncLocationDir + "MediaSync.exe"
-
 function Expand-ZipFile($file, $destination)
 {
-	$shell = new-object -com shell.application
-	$zip = $shell.NameSpace($file)
+    $shell = new-object -com shell.application
+    $zip = $shell.NameSpace($file)
+
   foreach($item in $zip.items()){
-    $shell.Namespace($destination).copyhere($item)
+    $shell.Namespace($destination).CopyHere($item, 0x14)
   }
+  
+  If (Test-Path "$destination\[Content_Types].xml"){
+	Remove-Item  "$destination\[Content_Types].xml"
+	}	
 }
 
 function Create-MediaSyncStartupShortcut($targetPath)
@@ -45,3 +25,27 @@ function Create-MediaSyncStartupShortcut($targetPath)
   $Shortcut.Save()  
 }
 
+#1. Download
+$mediaSyncDownloadUrl = "https://github.com/philoushka/MediaSync/releases/download/v1.1/MediaSync.zip"
+$mediaSyncLocationDir = "${env:ProgramFiles(x86)}" + "\MediaSync\"
+$mediaSyncZipLocalFullPath = $mediaSyncLocationDir + "MediaSync.zip"
+
+#Create the target directory if it doesn't exist
+if (!(Test-Path $mediaSyncLocationDir)){
+	New-Item $mediaSyncLocationDir -type directory -Force
+}
+	
+Write-Host "***Are you running As Administrator?****"
+#download the zip
+$wc = New-Object System.Net.WebClient
+$wc.DownloadFile($mediaSyncDownloadUrl,$mediaSyncZipLocalFullPath)
+
+#extract the zip
+Expand-ZipFile $mediaSyncZipLocalFullPath $mediaSyncLocationDir
+
+#create a shortcut for startup
+Create-MediaSyncStartupShortcut($mediaSyncLocalFullPath)
+
+#run the exe for the user
+Invoke-Item  "$mediaSyncLocationDir\MediaSync.exe"
+Write-Host "Done"
