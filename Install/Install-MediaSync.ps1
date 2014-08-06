2e6ed4c6dc360c7f0de65433ea14f346a536c428
Phil Campbell <philoushka@gmail.com>
2014-08-06 08:48:47 -0700

    extract a zip instead of single exe

--- Install/Install-MediaSync.ps1
+++ Install/Install-MediaSync.ps1
@@ -1,22 +1,47 @@
-#download the binary
+#1. Download the latest archive
+#2. Extract its contents into Program Files x86 \ MediaSync
+#3. Create a shortcut into current user's Startup dir
+#4. Run the exe
+
+#1. Download
 $mediaSyncDownloadUrl = "https://github.com/philoushka/MediaSync/releases/download/0.3/MediaSync.exe"
 $mediaSyncLocationDir = "${env:ProgramFiles(x86)}" + "\MediaSync\\"
-$file = "MediaSync.exe"
-$mediaSyncLocalFullPath = $mediaSyncLocationDir + $file
+$zipFile ="MediaSync.zip" 
+$mediaSyncZipLocalFullPath = $mediaSyncLocationDir + $zipFile
+
 #Create the target directory if it doesn't exist
 if (!(Test-Path $mediaSyncLocationDir)){
 	New-Item $mediaSyncLocationDir -type directory -Force
 }
 	
+#download the zip
 $wc = New-Object System.Net.WebClient
-$wc.DownloadFile($mediaSyncDownloadUrl,$mediaSyncLocalFullPath )
+$wc.DownloadFile($mediaSyncDownloadUrl,$mediaSyncZipLocalFullPath)
+
+#download its dependants
+Expand-ZipFile($mediaSyncZipLocalFullPath, $mediaSyncLocationDir)
 
 #create a shortcut for startup
-$WshShell = New-Object -comObject WScript.Shell
-$Startup =[System.Environment]::GetFolderPath("Startup");
-$Shortcut = $WshShell.CreateShortcut("$Startup\MediaSync.lnk")
-$Shortcut.TargetPath = $mediaSyncLocalFullPath
-$Shortcut.Save()
+Create-MediaSyncStartupShortcut($mediaSyncLocalFullPath)
 
-#run it
-Invoke-Item $mediaSyncLocalFullPath
+#run the exe for the user
+Invoke-Item $mediaSyncLocationDir + "MediaSync.exe"
+
+function Expand-ZipFile($file, $destination)
+{
+	$shell = new-object -com shell.application
+	$zip = $shell.NameSpace($file)
+  foreach($item in $zip.items()){
+    $shell.Namespace($destination).copyhere($item)
+  }
+}
+
+function Create-MediaSyncStartupShortcut($targetPath)
+{
+  $WshShell = New-Object -comObject WScript.Shell
+  $Startup =[System.Environment]::GetFolderPath("Startup");
+  $Shortcut = $WshShell.CreateShortcut("$Startup\MediaSync.lnk")
+  $Shortcut.TargetPath = $targetPath
+  $Shortcut.Save()  
+}
+
